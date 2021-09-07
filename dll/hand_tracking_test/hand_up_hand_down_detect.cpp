b56f35461743df7f784e375b2b331e22ca173d55
HW140701 <623256052@qq.com>
2021-09-07 09:32:27 +0800

    修改检测手臂放下、抬起的方法

--- dll/hand_tracking_test/hand_up_hand_down_detect.cpp
+++ dll/hand_tracking_test/hand_up_hand_down_detect.cpp
@@ -14,20 +14,43 @@ GoogleMediapipeHandTrackingDetect::HandUpHandDownDetect::~HandUpHandDownDetect()
 
 int GoogleMediapipeHandTrackingDetect::HandUpHandDownDetect::DetectHandUpOrHandDown(const std::vector<PoseInfo>& single_hand_joint_vector, int origin_image_height)
 {
-	if (single_hand_joint_vector.size() != 21)
-		return -1;
-
 	int result = -1;
 
-	int wrist_y = single_hand_joint_vector[0].y;
+	if (single_hand_joint_vector.size() != 21)
+		return result;
+	
 
-	if (fabs(origin_image_height - wrist_y) > (origin_image_height / 3))
+	int wrist_y = single_hand_joint_vector[0].y;
+	int middle_finger_tip_y = single_hand_joint_vector[12].y;
+	int diff_y = middle_finger_tip_y - wrist_y;
+
+	int half_origin_image_height = origin_image_height / 2;
+
+
+	if (diff_y > 0)
 	{
-		return HandUp_HandDown::HandUp;
+		if (wrist_y > half_origin_image_height)
+		{
+			return HandUp_HandDown::HandDown;
+		}
+		else
+		{
+			return HandUp_HandDown::HandUp;
+		}
 	}
 	else
 	{
-		return HandUp_HandDown::HandDown;
+		return HandUp_HandDown::HandUp;
 	}
+	//if (fabs(origin_image_height - wrist_y) > (origin_image_height / 3))
+	//{
+	//	return HandUp_HandDown::HandUp;
+	//}
+	//else
+	//{
+	//	return HandUp_HandDown::HandDown;
+	//}
+
+	return result;
 
 }
